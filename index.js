45ad5160e4c574438ed113f654a5280bd4fa3c61
khanfaizan123 <fk796346@gmail.com>
2024-07-08 23:06:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const { Server } = require('socket.io'); 
 
 const app = express();
 const server = http.createServer(app); // Create an HTTP server
